f4278ef1aa81f40def53b33ab37f339cd024d3fb
Coffeeteer <tgrieshiemer@gmail.com>
2017-01-31 17:44:02 -0500

    some files missing

---
+++ config/database.js
@@ -0,0 +1,12 @@
+// module.exports = {
+
+//     'url' : 'your-settings-here' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+// };
+
+// *** Sesquelize Database config *** \\
+//var sequelize = new Sequelize('database', 'username', null)
+
+module.exports = function(sequelize, DataTypes) {
+	
+};
