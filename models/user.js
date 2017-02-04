24e85dda254cf282020b98028bbf67e15405e44a
Coffeeteer <tgrieshiemer@gmail.com>
2017-02-03 22:46:46 -0500

    added sequelize user schema

---
+++ models/user.js
@@ -0,0 +1,55 @@
+
+// load the things we need
+var bcrypt   = require('bcrypt-nodejs');
+
+
+// define the schema for our user model
+//var userSchema = mongoose.Schema({});
+
+'use strict';
+module.exports = function(sequelize, DataTypes) {
+  var user = sequelize.define('user', {
+    
+        localemail: DataTypes.STRING(40),
+        localpassword: DataTypes.STRING(20),   
+  
+        fbid: DataTypes.STRING(40),
+        fbtoken: DataTypes.STRING(20),
+        fbemail: DataTypes.STRING(20),
+        fbname: DataTypes.STRING(20),
+
+        twitterid: DataTypes.STRING(20),
+        twittertoken: DataTypes.STRING(20),
+        twitterdisplayname: DataTypes.STRING(20),
+        twitterusername: DataTypes.STRING(20),
+
+        googleid: DataTypes.STRING(20),
+        googletoken: DataTypes.STRING(20),
+        googleemail: DataTypes.STRING(20),
+        googlename: DataTypes.STRING(20)
+    
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      }
+    }
+  });
+  return user;
+};
+
+
+
+// methods ======================
+// generating a hash
+userSchema.methods.generateHash = function(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+
+// checking if password is valid
+userSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.local.password);
+};
+
+// create the model for users and expose it to our app
+// module.exports = mongoose.model('User', userSchema);
