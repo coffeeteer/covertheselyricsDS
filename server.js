caa6998c7361d90b1f2d3aeef5dd9cc36fec6885
Coffeeteer <tgrieshiemer@gmail.com>
2017-02-20 20:58:05 -0500

    webpack issues

--- server.js
+++ server.js
@@ -11,6 +11,31 @@ var cookieParser    = require('cookie-parser');
 var session         = require('express-session');
 var webpack         = require('webpack');
 
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: './bin',
+        filename: 'app.bundle.js',
+    },
+    module: {
+        loaders: [{
+            test: /\.js?$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+        }]
+    },
+    plugins: [
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+            },
+            output: {
+                comments: false,
+            },
+        }),
+    ]
+}
+
 
 //console.log('process.env.PORT', process.env.PORT);
 var port            = process.env.PORT || 3010;
