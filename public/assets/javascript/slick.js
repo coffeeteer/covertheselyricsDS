065b5aaa28b1340df54a2eed4ff1753215d53512
Coffeeteer <tgrieshiemer@gmail.com>
2017-03-08 21:40:48 -0500

    working out the kinks of slick

--- public/assets/javascript/slick.js
+++ public/assets/javascript/slick.js
@@ -1,7 +1,27 @@
 $(document).ready(function() {
-	$('.multiple-items').slick({
-	  infinite: true,
-	  slidesToShow: 3,
-	  slidesToScroll: 3
-	});
+	// $('.lyric-container').slick({
+	//   infinite: true,
+	//   slidesToShow: 3,
+	//   slidesToScroll: 3
+	// });
+
+	$('#carousel').slick({
+    arrows: true,
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed:2500,
+    speed: 500,
+    variableWidth: true,
+    onAfterChange: function(){ 
+          var cat = ($('#carousel').slickCurrentSlide()) + 1;
+          $('.client-text > li').hide();
+          $('.client-text > li:nth-child('+ cat +')').show();
+    }
 });
+
+
+
+  $('.client-text > li').hide();
+  $('.client-text > li:first-child').show();
+});
+
