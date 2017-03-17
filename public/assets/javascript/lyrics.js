fdaa2861ace984336bbfb748ebe0549f40435f93
Coffeeteer <tgrieshiemer@gmail.com>
2017-03-16 21:16:36 -0400

    made the lyrics carousel 2 at time as per Daria Hunter

--- public/assets/javascript/lyrics.js
+++ public/assets/javascript/lyrics.js
@@ -1,26 +1,29 @@
 jQuery(document).ready(function($) {
-  // $('#lyricsCarousel', '#lyricsCarousel2').carousel();
-  // autoplay:true;
-  // this.interval: 8000;
+  (function(){
+	  // setup your carousels as you normally would using JS
+	  // or via data attributes according to the documentation
+	  // http://getbootstrap.com/javascript/#carousel
+	  $('#carousel123').carousel({ interval: 7000 });
+	  //$('#carouselABC').carousel({ interval: 3600 });
+	}());
 
-  	// Instantiate the Bootstrap carousel
-	$('.multi-item-carousel').carousel({
-	  interval: false
-	});
+	(function(){
+	  $('.carousel-showmanymoveone .item').each(function(){
+	    var itemToClone = $(this);
 
-	// for every slide in carousel, copy the next slide's item in the slide.
-	// Do the same for the next, next item.
-	$('.multi-item-carousel .lyric-item').each(function(){
-	  var next = $(this).next();
-	  if (!next.length) {
-	    next = $(this).siblings(':first');
-	  }
-	  next.children(':first-child').clone().appendTo($(this));
-	  
-	  if (next.next().length>0) {
-	    next.next().children(':first-child').clone().appendTo($(this));
-	  } else {
-	  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-	  }
-	});
+	    for (var i=1;i<2;i++) {
+	      itemToClone = itemToClone.next();
+
+	      // wrap around if at end of item collection
+	      if (!itemToClone.length) {
+	        itemToClone = $(this).siblings(':first');
+	      }
+
+	      // grab item, clone, add marker class, add to collection
+	      itemToClone.children(':first-child').clone()
+	        .addClass("cloneditem-"+(i))
+	        .appendTo($(this));
+	    }
+	  });
+	}());
 });
