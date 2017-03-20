fdd3f8642d2eb050e0e457c36b69b0735aeacd2b
Coffeeteer <tgrieshiemer@gmail.com>
2017-03-20 14:44:18 -0400

    active links in navbar has styling

--- public/assets/javascript/lyrics.js
+++ public/assets/javascript/lyrics.js
@@ -26,5 +26,18 @@ jQuery(document).ready(function($) {
 	    }
 	  });
 	}());
+	// ACtive Link on Navbar
+	// $(".nav a").on("click", function(){
+	//   $(".nav").find(".active").removeClass("active");
+	//   $(this).parent().addClass("active");
+	// });
 
+	var url = window.location;
+	// Will only work if string in href matches with location
+	//$('ul.nav a[href="'+ url +'"]').parent().addClass('active');
+
+	// Will also work for relative and absolute hrefs
+	$('ul.nav a').filter(function() {
+	    return this.href == url;
+	}).parent().addClass('active');
 });
